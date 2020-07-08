28a007c685c8baed4bb63d4bd7361fb682e35880
maulianSp <67595674+maulianSp@users.noreply.github.com>
2020-07-09 00:20:31 +0700

    tambah & tampilkan pertanyaan, tag
    - tabel migration baru 3 (users, questions, answers, tags)
    - auth middleware sudah bisa
    - sudah install file manager untuk editor

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,18 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
+});
+*/
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/','QuestionController@index');
+Route::get('/question/create','QuestionController@create')->middleware('auth');
+Route::post('/question/store','QuestionController@store')->middleware('auth');
+
+Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
+    \UniSharp\LaravelFilemanager\Lfm::routes();
 });
