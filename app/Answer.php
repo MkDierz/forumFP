2eed5a0cae9e0d1facfb3cf6d20acea32ac58055
maulianSp <67595674+maulianSp@users.noreply.github.com>
2020-07-11 11:18:03 +0700

    perbaikan controller untuk menghitung jawaban & komentar

--- app/Answer.php
+++ app/Answer.php
@@ -10,7 +10,7 @@ class Answer extends Model
     //
     public function questions()
     {
-        return $this->belongsTo('App\Question');
+        return $this->belongsTo('App\Question','untuk_pertanyaan_id','id');
     }
 
     public function answer_comments()
